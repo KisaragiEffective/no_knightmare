a34a347e7b5f49e9674ebd96c9433e4cffe3d004
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-02-12 14:51:46 +0900

    feat: support uBlock origin

---
+++ script/compile_ublockorigin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if ! which jq >/dev/null; then
+  echo "jq is required" >&2
+  exit 1
+fi
+
+if ! which git >/dev/null; then
+  echo "git is required" >&2
+  exit 1
+fi
+
+dist="$(dirname "$0")/../ublockorigin.txt"
+if [ -f "$dist" ]; then
+  rm "$dist"
+fi
+
+output() {
+  echo "$1" >> "$dist"
+}
+
+output "! This blacklist is powered by https://github.com/KisaragiEffective/no_knightmare"
+output "! Generated-Date: $(date -u +%Y-%m-%dT%H:%M:%SZ)"
+output "! Git-Commit: $(git rev-parse HEAD)"
+
+data="$(dirname "$0")/../data.json"
+jq -r '.[] | select(.type == "domain") | .domain | ("||" + . + "^")' < "$data" >> "$dist"
+jq -r '.[] | select(.type == "path") | .path | ("||" + . + "^")' < "$data" >> "$dist"
